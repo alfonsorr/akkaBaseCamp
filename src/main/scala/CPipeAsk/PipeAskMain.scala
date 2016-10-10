46f168d6c4256b5e1af7e40d2d06baae8d903eb3
Alfonso <roaredalfonso@gmail.com>
2016-10-10 23:00:06 +0200

    modifications

--- src/main/scala/CPipeAsk/PipeAskMain.scala
+++ src/main/scala/CPipeAsk/PipeAskMain.scala
@@ -7,13 +7,6 @@ object PipeAskMain extends App {
   val system = ActorSystem("ask")
 
   var wordList1 = List("Let", "It", "Crash")
-  var wordList2 = (for {
-    l1 <- 'a' to 'z'
-    l2 <- 'a' to 'z'
-    l3 <- 'a' to 'z'
-    l4 <- 'a' to 'z'
-  } yield s"$l1$l2$l3$l4"
-    ).toList
 
   val actorCoord = system.actorOf(ActorCoordinator.props)
 
@@ -22,13 +15,6 @@ object PipeAskMain extends App {
   scala.io.StdIn.readLine()
 
   actorCoord ! SetWords(wordList1)
-  actorCoord ! Start
-  actorCoord ! CleanList
-
-  println("enter to continue")
-  scala.io.StdIn.readLine()
-
-  actorCoord ! SetWords(wordList2)
   actorCoord ! Start
   actorCoord ! CleanList
 
